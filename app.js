2eb3e58680e6c171e0a224dceb71e62ff8f8b107
20JorgeCardenas05 <jcardenas74@ucol.mx>
2023-11-15 10:21:04 -0600

    Secon commit

--- app.js
+++ app.js
@@ -70,4 +70,81 @@ function dividir() {
 
 function borrar() {
     document.getElementById("formulario").reset();
+    sumaa.innerHTML = "";
+    restaa.innerHTML = "";
+    multii.innerHTML = "";
+    divv.innerHTML = "";
 }
+
+const boton = document.querySelector(".botonclick");
+function color() {
+    boton.classList.toggle('negativo');
+    console.log("Diste click en el botón.");
+}
+
+boton.addEventListener("click", color);
+
+boton.addEventListener("mouseover", () => {
+    console.log("Estoy encima del botón.");
+});
+boton.addEventListener("mouseout", () => console.log("Estoy fuera del botón."));
+
+window.addEventListener('keydown', (e) => {
+    console.log("Pulsó una tecla.");
+    console.log(String.fromCharCode(e.keyCode));
+});
+window.addEventListener('keypress', (e) => {
+    console.log("Presionó una tecla.");
+    console.log(String.fromCharCode(e.keyCode));
+});
+window.addEventListener('keyup', (e) => {
+    console.log("Dejó de presionar una tecla.");
+    console.log(String.fromCharCode(e.keyCode));
+});
+window.addEventListener('load', () => {
+    console.log("Terminé de cargar la página.");
+});
+
+const agregar = document.getElementById("agregar");
+
+agregar.addEventListener("click", () => {
+    let texto = document.getElementById("texto").value;
+    areatexto.innerHTML += texto;
+    document.getElementById("textoo").reset();
+});
+
+const su = document.getElementById("su");
+const re = document.getElementById("re");
+const mu = document.getElementById("mu");
+const di = document.getElementById("di");
+
+su.addEventListener('mouseover', () => {
+    su.classList.add("ver");
+});
+su.addEventListener('mouseout', () => {
+    su.classList.remove("ver");
+});
+re.addEventListener('mouseover', () => {
+    re.classList.add("ver");
+});
+re.addEventListener('mouseout', () => {
+    re.classList.remove("ver");
+});
+mu.addEventListener('mouseover', () => {
+    mu.classList.add("ver");
+});
+mu.addEventListener('mouseout', () => {
+    mu.classList.remove("ver");
+});
+di.addEventListener('mouseover', () => {
+    di.classList.add("ver");
+});
+di.addEventListener('mouseout', () => {
+    di.classList.remove("ver");
+});
+borrrar.addEventListener('mouseover', () => {
+    borrrar.classList.add("verb");
+});
+borrrar.addEventListener('mouseout', () => {
+    borrrar.classList.remove("verb");
+});
